017461d3746f49be20843af66deb4c08ba8e4443
thedonwind69@gmail.com <thedonwind69@gmail.com>
2023-05-02 11:20:41 -0500

    updated users controller to GET all users

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -30,3 +30,14 @@ export const updateUser = async (req, res) => {
     }
 
 };
+
+// GET ALL users for search bar functionality
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const allUsers = await User.find();
+        res.status(200).json(allUsers);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+}
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getUser, updateUser } from "../controllers/user.js";
+import { getUser, updateUser, getAllUsers } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.get('/:username', getUser)
+
+router.get('/', getAllUsers)
 
 router.put('/:username', updateUser)
 
